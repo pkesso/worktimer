ac57d692c5131f8a46b5c6a141e23ab7b5daf8f8
Your Name <you@example.com>
2024-10-11 17:40:34 +0300

    preserve running/paused state

--- worktimer.sh
+++ worktimer.sh
@@ -8,16 +8,13 @@
 
 # TODO fix reset counter on date change
 # TODO human-readable date in statefile?
-# TODO colored panel?
-
-# start paused
-PAUSE=1
 
 # 
 config() {
     if [ "$DEBUG" ]; then echo 'Configuring'; fi
     DEFAULT_WORKSECONDS="$(( 8*3600 ))"    # 8h
     DEFAULT_STATEFILE="$HOME/.worktimer.state"
+    DEFAULT_PAUSEFILE="$HOME/.worktimer.pause"
     DEFAULT_PIDFILE="/run/user/$UID/worktimer.pid"
     
     if [ -z "$WORKTIMER_WORKSECONDS" ]
@@ -34,6 +31,13 @@ config() {
         STATEFILE="$WORKTIMER_STATEFILE"
     fi
 
+    if [ -z "$WORKTIMER_PAUSEFILE" ]
+    then
+        PAUSEFILE="$DEFAULT_PAUSEFILE"
+    else
+        PAUSEFILE="$WORKTIMER_PAUSEFILE"
+    fi
+
     if [ -z "$WORKTIMER_PIDFILE" ]
     then
         PIDFILE="$DEFAULT_PIDFILE"
@@ -42,18 +46,30 @@ config() {
     fi
 }
 
+# check if was paused
+read-pause() {
+    if [ "$DEBUG" ]; then echo 'Trying to read pause file'; fi
+    if [ -f "$PAUSEFILE" ]
+    then
+        if [ "$DEBUG" ]; then echo 'Pause file found, set PAUSE=1'; fi
+        PAUSE=1
+    else
+        if [ "$DEBUG" ]; then echo 'Pause file not found, set PAUSE=0'; fi
+        PAUSE=0
+    fi
+}
+
 # try to use previous data from state file
-try-resume() {
+read-state() {
     if [ "$DEBUG" ]; then echo 'Trying to resume from state file'; fi
     if [ -f "$STATEFILE" ]
     then
         STATE=$(cat "$STATEFILE") || return 0
         STATETIME=$(echo "$STATE" | cut -f 1 -d ' ') || return 0
         STATEWORKSECONDS=$(echo "$STATE" | cut -f 2 -d ' ') || return 0
-        # echo "Statetime $STATETIME Stateworkseconds $STATEWORKSECONDS"
         if [ "$DEBUG" ]
             then
-            echo "State in file found: STATETIME $(date -d @"$STATETIME" +%Y/%m/%d_%H-%M-%S) STATEWORKSECONDS $(date "+%s") $WORKSECONDS"
+            echo "State in file found: STATETIME $(date -d @"$STATETIME" +%Y/%m/%d_%H-%M-%S) STATEWORKSECONDS $WORKSECONDS"
         fi
         if [ "$(date -d @"$STATETIME" +%Y%m%d)" == "$(date +%Y%m%d)"  ]
         then
@@ -68,16 +84,17 @@ try-resume() {
 }
 
 # invert pause
-pause-unpause() {
+flip-pause() {
     # send SIGUSR1 to the process to pause timer
     # kill -s SIGUSR1 $(cat $PIDFILE)
     ((PAUSE ^= 1 ))
     if [ $PAUSE -eq 0 ]
     then
         if [ "$DEBUG" ]; then echo 'Unpausing'; fi
-        try-resume
+        rm -f "$PAUSEFILE"
     else
         if [ "$DEBUG" ]; then echo 'Pausing'; fi
+        touch "$PAUSEFILE"
     fi
 }
 
@@ -127,6 +144,8 @@ main() {
 
 if [ "$DEBUG" ]; then echo 'DEBUG is on'; fi
 config
-trap pause-unpause SIGUSR1
+read-pause
+read-state
+trap flip-pause SIGUSR1
 trap cleanup SIGINT
 main
